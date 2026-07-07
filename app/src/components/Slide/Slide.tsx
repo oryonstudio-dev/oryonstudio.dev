9acc31b0db64e27700427f2d6e48612d5556059f
Cuby92 <jakub.bartul@hotmail.com>
2026-07-07 11:45:36 +0200

    Create Slide component

---
+++ app/src/components/Slide/Slide.tsx
@@ -0,0 +1,18 @@
+import styles from './Slide.module.scss';
+import { Children, AnyRef } from '@/utils/types';
+
+const s = styles;
+
+interface Props extends Children {
+    className?: string;
+    ref?: AnyRef;
+    id?: string;
+}
+
+function Slide({ children, className, ref, id } : Props) {
+    return (
+        <section className={`${className} ${s.slide}`} ref={ref} id={id}>
+            {children}
+        </section>
+    );
+}
