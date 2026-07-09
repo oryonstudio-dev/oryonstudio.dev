1d4be8c36360479779f9148e270bbc7bd4808737
Cuby92 <jakub.bartul@hotmail.com>
2026-07-09 12:45:51 +0200

    Add footer links array

--- app/src/components/Footer/Footer.tsx
+++ app/src/components/Footer/Footer.tsx
@@ -1,8 +1,17 @@
 import styles from "./Footer.module.scss";
+import { LinkTemplate } from '@/utils/types';
 
 const s = styles;
 
 function Footer() {
+    const links: LinkTemplate[] = [
+        { href: '/', label:'home' },
+        { href: '/about', label: 'about_us' },
+        { href: '/experience', label: 'our_experience' },
+        { href: '/services', label: 'our_services' },
+        { href: '/contact', label: 'contact_us'}
+    ];
+
     return (
         <footer className={s.Footer}></footer>
     );
