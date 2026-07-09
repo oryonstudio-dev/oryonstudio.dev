88d32b678b80526fd51f0acd0e41f31c011db735
Cuby92 <jakub.bartul@hotmail.com>
2026-07-09 21:02:35 +0200

    Implement on pages and add locales

--- app/src/app/[locale]/contact/page.tsx
+++ app/src/app/[locale]/contact/page.tsx
@@ -1,12 +1,15 @@
 import styles from './page.module.scss';
 import Slide from '@/components/Slide/Slide';
+import { useTranslations } from 'next-intl';
 
 const s = styles;
 
 function Contact() {
+    const t = useTranslations('contact.content');
+
     return (
         <Slide className={s.Hero}>
-          <h1>page under construction <span className={s.pointer}>█</span></h1>
+          <h1>{ t('page under construction') } <span className={s.pointer}>█</span></h1>
         </Slide>
     );
 }
