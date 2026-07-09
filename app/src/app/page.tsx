053b2602e3822392c1e5a71c11517c57e2396af2
Cuby92 <jakub.bartul@hotmail.com>
2026-07-09 17:14:22 +0200

    Add 'page under construction' heading

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,6 +1,7 @@
 import styles from './page.module.scss';
 import ScrollSmootherWrapper from '@/utils/gsap/ScrollSmoother';
 import Footer from '@/components/Footer/Footer';
+import Slide from '@/components/Slide/Slide';
 
 const s = styles;
 
@@ -8,7 +9,9 @@ function Home() {
   return (
     <ScrollSmootherWrapper>
       <main className="Main">
-
+        <Slide className={s.Hero}>
+          <h1>page under construction <span className={s.pointer}>█</span></h1>
+        </Slide>
       </main>
       <Footer />
     </ScrollSmootherWrapper>
