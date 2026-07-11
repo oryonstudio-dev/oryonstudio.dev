951d2967132fd2fa4730bd3f08b9df52ffcb9fd9
Cuby92 <jakub.bartul@hotmail.com>
2026-07-11 13:16:53 +0200

    Add internationalized metadata to services page

--- app/src/app/[locale]/services/layout.tsx
+++ app/src/app/[locale]/services/layout.tsx
@@ -1,6 +1,17 @@
 import { Children } from '@/utils/types';
 import ScrollSmootherWrapper from '@/utils/gsap/ScrollSmoother';
 import Footer from '@/components/Footer/Footer';
+import { Metadata } from 'next';
+import { routing } from '@/i18n/routing';
+import { MetadataParams, Locale } from '@/i18n/types';
+import { readMetadata } from '@/i18n/functions';
+import { notFound } from 'next/navigation';
+
+export async function generateMetadata({ params }: MetadataParams): Promise<Metadata> {
+    const { locale } = await params;
+    if (!routing.locales.includes(locale as Locale)) notFound();
+    return readMetadata('services');
+}
 
 function Layout({ children }: Children) {
     return (
