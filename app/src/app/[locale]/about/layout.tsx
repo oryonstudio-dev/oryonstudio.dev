d00820bd7420ef97001a9a5d63892454902e353f
Cuby92 <jakub.bartul@hotmail.com>
2026-07-15 19:58:20 +0200

    Add ScrollSmoother dynamic import

--- app/src/app/[locale]/about/layout.tsx
+++ app/src/app/[locale]/about/layout.tsx
@@ -1,9 +1,11 @@
 import { Children } from '@/utils/types';
-import ScrollSmootherWrapper from '@/utils/gsap/ScrollSmoother';
 import Footer from '@/components/Footer/Footer';
 import { MetadataParams } from '@/i18n/types';
 import { Metadata } from 'next';
 import { readMetadata, checkLocale } from '@/i18n/functions';
+
+import dynamic from 'next/dynamic';
+const ScrollSmootherWrapper = dynamic(() => import('@/utils/gsap/ScrollSmoother'), { ssr: false });
 
 export async function generateMetadata({ params }: MetadataParams): Promise<Metadata> {
     const { locale } = await params;
