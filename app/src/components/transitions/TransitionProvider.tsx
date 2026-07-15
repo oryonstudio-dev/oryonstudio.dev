548b34dc56b9808efb5e35344d8c495848642dfa
Cuby92 <jakub.bartul@hotmail.com>
2026-07-15 14:02:06 +0200

    Add branding to the transition

--- app/src/components/transitions/TransitionProvider.tsx
+++ app/src/components/transitions/TransitionProvider.tsx
@@ -1,19 +1,22 @@
 'use client';
 
 import { TransitionRouter } from 'next-transition-router';
-import { Children, DivRef, Div } from '@/utils/types';
+import { Children, Div, DivRef, H, HRef, Any, AnyRef } from '@/utils/types';
 import { useRef } from 'react';
 import styles from './TransitionProvider.module.scss';
 import { gsap } from 'gsap';
 import { mapArray } from '@/utils/functions';
 
 const s = styles;
+
 const rows = 4;
 const eases = ['power1.out', 'power2.out', 'power3.out', 'power4.out'];
 
 function TransitionProvider({ children }: Children) {
-    const gridRef: DivRef = useRef(null);
-    const blocksRefs: DivRef[] = Array.from({ length: rows }, () => useRef(null));
+    const gridRef:    DivRef               = useRef<Div>(null);
+    const blocksRefs: DivRef[]             = Array.from({ length: rows }, () => useRef<Div>(null));
+    const headingRef: HRef                 = useRef<H>(null);
+    const wordsRefs:  AnyRef[]             = Array.from({ length: rows }, () => useRef<Any>(null));
 
     const animate: {
         in:  (onComplete: () => void) => gsap.core.Timeline;
@@ -21,12 +24,17 @@ function TransitionProvider({ children }: Children) {
     } = {
         in: (onComplete: () => void) => {
             const tl = gsap.timeline({ onComplete });
-            const blocks = mapArray(blocksRefs)
+            const blocks = mapArray(blocksRefs);
+            const words  = mapArray(wordsRefs);
 
             tl.set(blocks, {
                 transformOrigin: 'left center',
                 scaleX: 0
             });
+
+            tl.set(words, {
+                y: '100%'
+            })
 
             blocks.forEach((block) => {
                 tl.to(block, {
@@ -36,16 +44,35 @@ function TransitionProvider({ children }: Children) {
                 }, '<0.1');
             });
 
+            tl.to(words, {
+                y: 0,
+                stagger: 0.1,
+                duration: 1,
+                ease: 'power4.out'
+            }, '<');
+
             return tl;
         },
 
         out: (onComplete: () => void) => {
             const tl = gsap.timeline({ onComplete });
-            const blocks = mapArray(blocksRefs)
+            const blocks = mapArray(blocksRefs);
+            const words  = mapArray(wordsRefs);
 
             tl.set(blocks, {
                 transformOrigin: 'right center',
                 scaleX: 1
+            });
+
+            tl.set(mapArray(wordsRefs), {
+                y: 0
+            });
+
+            tl.to(words, {
+                y: '100%',
+                duration: 0.5,
+                stagger: 0.1,
+                ease: 'power1.out'
             });
 
             blocks.forEach((block) => {
@@ -54,7 +81,7 @@ function TransitionProvider({ children }: Children) {
                     duration: 1,
                     ease: gsap.utils.random(eases),
                 }, '<0.1');
-            });
+            }, '<0.9');
             
             return tl;
         }
@@ -80,6 +107,10 @@ function TransitionProvider({ children }: Children) {
                         ref={blocksRefs[i]}
                     />
                 )) }
+
+                <div className={s.branding}>
+                    <h1 ref={headingRef}><span className={s.oryon} ref={wordsRefs[0]}>ORYON</span><span className={s.studio} ref={wordsRefs[1]}>STUDIO</span></h1>
+                </div>
             </div>
             { children }
         </TransitionRouter>
