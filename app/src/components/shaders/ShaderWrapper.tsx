0dc1c502b2d6db189a04dcab2664e54a3daef044
Cuby92 <jakub.bartul@hotmail.com>
2026-07-17 15:18:53 +0200

    Add placeholder pass to shaders

--- app/src/components/shaders/ShaderWrapper.tsx
+++ app/src/components/shaders/ShaderWrapper.tsx
@@ -2,19 +2,24 @@
 
 import { useDeviceSpecs } from '@/utils/functions';
 import { useState, useEffect } from 'react';
+import Image from 'next/image';
 
 import dynamic from 'next/dynamic';
+import { StaticImport } from 'next/dist/shared/lib/get-img-props';
 const Shader = dynamic(() => import('shaders/react').then(mod => mod.Shader), { ssr: false });
 
 interface Props {
-    children: React.ReactNode,
-    width?: string,
-    height?: string,
-    style?: React.CSSProperties,
-    center?: boolean
+    children:          React.ReactNode;
+    width?:            string;
+    height?:           string;
+    style?:            React.CSSProperties;
+    center?:           boolean;
+    imagePlaceholder?: string | StaticImport;
+    video?:            string | undefined;
+    videoPlaceholder?: string | undefined;
 }
 
-function ShaderWrapper({ children, width = '110vw', height = '110vh', style = {}, center = true }: Props) {
+function ShaderWrapper({ children, width = '110vw', height = '110vh', style = {}, center = true, imagePlaceholder = '', video ='', videoPlaceholder = '' }: Props) {
     const [mounted, setMounted] = useState<boolean>(false);
     const specs = useDeviceSpecs();
 
@@ -29,6 +34,55 @@ function ShaderWrapper({ children, width = '110vw', height = '110vh', style = {}
         transform: 'translate(-50%, -50%)'
     } : {};
 
+    function shader() {
+        if (!specs.lowPowerDevice) {
+            return (
+                <Shader 
+                    style={{ 
+                        width: '100%',
+                        height: '100%'
+                    }}
+                >
+                    { children }
+                </Shader>
+            );
+        } else return '';
+    }
+
+    function placeholder() {
+        if (specs.lowPowerDevice) {
+            if (videoPlaceholder) {
+                return (
+                    <video
+                        src={video}
+                        poster={videoPlaceholder}
+                        autoPlay
+                        muted
+                        loop
+                        playsInline
+                        preload="metadata"
+                        style={{ objectFit: 'cover', position: 'absolute', inset: 0 }}
+                    />
+                );
+            } else if (imagePlaceholder) {
+                return (
+                    <Image
+                        placeholder="blur"
+                        src={imagePlaceholder}
+                        fill={true}
+                        alt=""
+                    />
+                );
+            } else return undefined;
+        } else return undefined;
+    }
+
+    function content() {
+        if      (shader())      return shader();
+        else if (placeholder()) return placeholder();
+        else                    return '';
+    }
+
     return (
         <div
             style={{
@@ -41,16 +95,7 @@ function ShaderWrapper({ children, width = '110vw', height = '110vh', style = {}
                 ...style
             }}
         >
-            { specs.lowPowerDevice ? '' : (
-                <Shader 
-                    style={{ 
-                        width: '100%',
-                        height: '100%'
-                    }}
-                >
-                    { children }
-                </Shader>
-            )}
+            { content() }
         </div>
     )
 }
