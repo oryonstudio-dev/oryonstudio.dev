977bb5a5d805c6e6cef1b85cbc2f1f3426bca0de
Cuby92 <jakub.bartul@hotmail.com>
2026-07-20 19:48:23 +0200

    Convert GSAP to CSS

--- app/src/components/loading/LoadingPage.tsx
+++ app/src/components/loading/LoadingPage.tsx
@@ -1,80 +1,24 @@
 'use client';
 
-import { Children, El } from '@/utils/types';
-import { useRef, useEffect } from 'react';
+import { Children } from '@/utils/types';
 import styles from './LoadingPage.module.scss';
-import { gsap } from 'gsap';
 
 const s = styles;
-
 const ROWS = 4;
-const EASES = ['power1.out', 'power2.out', 'power3.out', 'power4.out'];
 
 function LoadingPage({ children }: Children) {
-    const gridRef    = useRef <El.Div> (null);
-    const headingRef = useRef <El.H>   (null);
-    const blocksRef  = useRef <(El.Div | null)[]> ([]);
-    const wordsRef   = useRef <(El     | null)[]> ([]);
-    
-    function animate() {
-        const blocks = blocksRef.current.filter(Boolean);
-        const words  = wordsRef .current.filter(Boolean);
-
-        const tl = gsap.timeline();
-
-        tl.set(blocks, {
-            transformOrigin: 'right center',
-            scaleX: 1
-        });
-
-        tl.set(words, {
-            y: 0
-        });
-
-        tl.to(words, {
-            y: '100%',
-            duration: 0.5,
-            stagger: {
-                each: 0.1,
-                from: 'end'
-            },
-            ease: 'power1.out'
-        });
-
-        blocks.forEach(block => {
-            tl.to(block, {
-                scaleX: 0,
-                duration: 1,
-                ease: EASES[Math.floor(Math.random() * EASES.length)],
-            }, '<0.1');
-        }, '<0.9');
-        
-        return tl;
-    }
-
-    useEffect(() => {
-        animate();
-    }, []);
-
     return (
         <>
-            <div className={s.grid} ref={gridRef}>
+            <div className={s.grid}>
                 { Array.from({ length: ROWS }).map((_, i) => (
-                    <div 
-                        key={i}
-                        className={s.block}
-                        ref={el => { blocksRef.current[i] = el }}
-                    />
+                    <div key={i} className={s.block} />
                 )) }
 
                 <div className={s.branding}>
-                    <h1 ref={headingRef}>
-                        <span className={s.oryon} ref={ el => { wordsRef.current[0] = el }}>
-                            ORYON
-                        </span>
-                        <span className={s.studio} ref={el => { wordsRef.current[1] = el }}>
-                            STUDIO</span>
-                        </h1>
+                    <h1>
+                        <span className={s.oryon} >ORYON</span>
+                        <span className={s.studio}>STUDIO</span>
+                    </h1>
                 </div>
             </div>
             { children }
