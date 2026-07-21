6d11fa87844a584d832e32677aafdf66da5522ad
Cuby92 <jakub.bartul@hotmail.com>
2026-07-21 22:25:33 +0200

    Add size

--- app/src/components/shaders/GlassRect.tsx
+++ app/src/components/shaders/GlassRect.tsx
@@ -4,8 +4,8 @@ import { Shader, Checkerboard, Glass } from 'shaders/react'
 
 export default function ShaderEffect() {
   return (
-    <Shader style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' }}>
-        <Glass />
+    <Shader style={{ position: 'fixed', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '500px', aspectRatio: 1 }}>
+        <Glass scale={1} />
     </Shader>
   )
 }
