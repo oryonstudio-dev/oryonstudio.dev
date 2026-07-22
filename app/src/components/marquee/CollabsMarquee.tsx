69033068606bf228d5f052fe1b91d2c85645faca
Cuby92 <jakub.bartul@hotmail.com>
2026-07-22 12:23:22 +0200

    Add text to marquee

--- app/src/components/marquee/CollabsMarquee.tsx
+++ app/src/components/marquee/CollabsMarquee.tsx
@@ -6,17 +6,23 @@ const s = styles;
 
 function CollabsMarquee({ className }: { className?: string }) {
     return (
-        <Marquee className={className}>
-            <Image className={s.item} width={750}  height={301} alt="Biogas Solutions"    src="https://www.biogassolutions.pl/img/brand/logo.svg" />
-            <Image className={s.item} width={1734} height={540} alt="Lumi"                src="/collabs/lumi.png"                                 />
-            <Image className={s.item} width={1288} height={158} alt="SIMERIS RACING"      src="/collabs/simeris-racing.svg"                       />
-            <Image className={s.item} width={440}  height={212} alt="Parysek Investments" src="/collabs/parysek-investments.svg"                  />
-            <div className={s.item}>
-                <Image className={s.img} width={688}  height={230} alt="RBE" src="/collabs/rbe.svg" />
-                <div className={s.divider} style={{ background: '#005e39' }}></div>
-                <p style={{ color: '#005e39' }}>Romgos Bio Energia</p>
+        <div className={`${s.collabsMarquee} ${className}`}>
+            <div className={s.text}>
+                <p className={s.black}>We've worked with</p>
+                <p className={s.white}>We've worked with</p>
             </div>
-        </Marquee>
+            <Marquee>
+                <Image className={s.item} width={750}  height={301} alt="Biogas Solutions"    src="https://www.biogassolutions.pl/img/brand/logo.svg" />
+                <Image className={s.item} width={1734} height={540} alt="Lumi"                src="/collabs/lumi.png"                                 />
+                <Image className={s.item} width={1288} height={158} alt="SIMERIS RACING"      src="/collabs/simeris-racing.svg"                       />
+                <Image className={s.item} width={440}  height={212} alt="Parysek Investments" src="/collabs/parysek-investments.svg"                  />
+                <div className={s.item}>
+                    <Image className={s.img} width={688}  height={230} alt="RBE" src="/collabs/rbe.svg" />
+                    <div className={s.divider} style={{ background: '#005e39' }}></div>
+                    <p style={{ color: '#005e39' }}>Romgos Bio Energia</p>
+                </div>
+            </Marquee>
+        </div>
     );
 }
 
